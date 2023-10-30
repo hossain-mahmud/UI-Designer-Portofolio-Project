44e7daaf441f3cad6b99a398a6dcb30911184afd
hossain <hossainmahmud.cse@gmail.com>
2023-10-31 01:27:01 +0600

    add project part

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+// nav-fiexd start
+let navMenu=document.querySelector('.nav_menu');
+window.addEventListener('scroll',()=>{
+    let scrolling=this.scrollY;
+    if(scrolling > 160){
+        navMenu.classList.add('fiexd_menu');
+    }
+    else{
+        navMenu.classList.remove('fiexd_menu');
+    }
+})
 
+// aos animation start
+AOS.init();
+
+// counter start 
+$('.count').counterUp({
+    delay: 10,
+    time: 2000
+});
