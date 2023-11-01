6fd29cb4418d0ab0d0bd5f40bfa0eb66d6406a62
hossain <hossainmahmud.cse@gmail.com>
2023-11-02 02:46:28 +0600

    add testimonial part

--- js/script.js
+++ js/script.js
@@ -18,3 +18,38 @@ $('.count').counterUp({
     delay: 10,
     time: 2000
 });
+
+// testimonial cards slider start
+$('.testimonial_cards').slick({
+    dots: true,
+    autoplay:true,
+    speed: 1000,
+    arrows: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
